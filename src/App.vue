dfa9e01b7af5b25994775698aa274cc097f46695
to and fro <32184942224.qq.com>
2024-02-29 09:32:49 +0800

    计数器案例项目基本结构

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
-
+    <my-addition></my-addition>
+    <p>----------------------------------------</p>
+    <my-substraction></my-substraction>
   </div>
 </template>
 
 <script>
+import Addition from './components/Additon.vue'
+import Subtraction from './components/Subtraction.vue'
 export default {
+  components: {
+    'my-addition': Addition,
+    'my-substraction': Subtraction
+  },
   data() {
     return {}
   }
